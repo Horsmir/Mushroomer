6f054b86fcd410e8ab2ba75b85f5bef9ca8c1226
Roman Braun <horsmir@yandex.ru>
2020-03-18 12:30:19 +0900

    First release android demo

--- src/levels/Level01.gd
+++ src/levels/Level01.gd
@@ -4,3 +4,4 @@ extends Node2D
 func _ready():
     PlayerData.current_level_path = filename
     PlayerData.save_game()
+
--- src/screens/MainScreen.gd
+++ src/screens/MainScreen.gd
@@ -2,6 +2,4 @@ extends Control
 
 
 func _ready():
-    var save_file = File.new()
-    if save_file.file_exists(PlayerData.save_file_name):
-        $Menu/ContinueButton.disabled = false
+    pass
